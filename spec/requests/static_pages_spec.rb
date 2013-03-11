31f63846ce7480463ef526f5d6251acafc36942b
Kathleen Wu <kathleen.wu@yale.edu>
2013-03-11 19:07:42 -0400

    Added Rspec let function

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+let(:base_title) { "Ruby on Rails Tutorial Sample App"}
+
   
   describe "Home page" do
   	it "should have the h1 'Sample App'" do
@@ -11,7 +14,7 @@ describe "StaticPages" do
     it "should have the right title 'Home'" do
     visit '/static_pages/home'
     page.should have_selector('title', 
-      :text => "Ruby on Rails Tutorial Sample App | Home")
+      :text => "#{base_title} | Home")
     end 
   end 
 
@@ -25,7 +28,7 @@ describe "StaticPages" do
     it "should have the right title 'Help'" do
     visit '/static_pages/help'
     page.should have_selector('title',
-      :text => "Ruby on Rails Tutorial Sample App | Help")
+      :text => "#{base_title} | Help")
   	end
   end
 
@@ -38,7 +41,7 @@ describe "StaticPages" do
     it "should have the right title 'About Us'" do
     visit '/static_pages/about'
     page.should have_selector('title', 
-      :text => "Ruby on Rails Tutorial Sample App | About Us")
+      :text => "#{base_title} | About Us")
   	end
   end
 
@@ -50,7 +53,7 @@ describe "StaticPages" do
 
       it "should have the right title 'Contact'" do 
         visit '/static_pages/contact'
-        page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
+        page.should have_selector('title', :text => "#{base_title} | Contact")
       end 
       
     end
